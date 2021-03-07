b464cb2842eb93ca0fa4cfc100b4d9fc0c95f907
Nathan Hild <nathan.hild@gmail.com>
2021-03-07 19:04:14 +0100

    automatically download ezdxf

--- __init__.py
+++ __init__.py
@@ -2,6 +2,10 @@
 Base module required for Blender registration
 """
 
+import subprocess
+import pkg_resources
+import bpy
+import sys
 from . import auto_load
 
 bl_info = {
@@ -16,8 +20,22 @@ bl_info = {
     "category": "Import-Export"
 }
 
-al = auto_load.AutoLoad()
 
+def ensure_ezdxf():
+    # Check if module is installed : https://stackoverflow.com/a/44210735/7092409
+    # Also https://devtalk.blender.org/t/can-3rd-party-modules-ex-scipy-be-installed-when-an-add-on-is-installed/9709/10
+    if "ezdxf" not in {pkg.key for pkg in pkg_resources.working_set}:
+        # Install 3rd party modules : https://blender.stackexchange.com/a/153520/86891
+        py_exec = sys.executable
+        # ensure pip is installed & update
+        subprocess.call([str(py_exec), "-m", "ensurepip", "--user"])
+        subprocess.call([str(py_exec), "-m", "pip", "install", "--upgrade", "pip"])
+        subprocess.call([str(py_exec),"-m", "pip", "install", f"--target={str(py_exec)[:-14]}" + "lib", "ezdxf"])
+        if "ezdxf" in {pkg.key for pkg in pkg_resources.working_set}:
+            print(" - DXF Exporter : ezdxf installed. Good to go !")
+
+ensure_ezdxf()
+al = auto_load.AutoLoad()
 
 def register():
     al.register()
