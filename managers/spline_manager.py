f71eb96b26d7dd2f5267fa5d85ee33479f0a8f3b
Nathan Hild <nathan.hild@gmail.com>
2021-03-17 10:29:04 +0100

    Fix Spline and Text Creation callbacks

--- managers/spline_manager.py
+++ managers/spline_manager.py
@@ -5,11 +5,9 @@ from .manager import Manager
 class SplineManager(Manager):
     def write_curve(self, layout, curve_obj, matrix, raa, dxfattribs, callback):
         curve = curve_obj.data
-        def func_spline(spl):
-            if len(spl.points) > 0:
-                return layout.add_spline([(matrix @ p.co)[0:3] for p in spl.points])
-            else:
-                return layout.add_spline([matrix @ p.co for p in spl.bezier_points])
-
         for spline in curve.splines:
-            self.create_and_transform_entity(lambda: func_spline(spline), True, dxfattribs, callback)
+            if len(spline.points) > 0:
+                spline = layout.add_spline([(matrix @ p.co)[0:3] for p in spline.points])
+            else:
+                spline = layout.add_spline([matrix @ p.co for p in spline.bezier_points])
+            callback(spline)
--- managers/text_manager.py
+++ managers/text_manager.py
@@ -22,26 +22,8 @@ class TextManager(Manager):
 
         scale = matrix.to_scale()
         # TODO : Use correct font : Need to know supported ones else file is corrupted
-
-        def entity_func_text():
-            align_dxf = ""
-            if align_y in ('TOP', 'BOTTOM'):
-                align_dxf = align_y + "_"
-            elif align_y == 'CENTER':
-                align_dxf = "MIDDLE_"
-            if align_x in ('LEFT', 'CENTER', 'RIGHT'):
-                align_dxf += align_x
-            else:
-                align_dxf += "CENTER"
-            dxfattribs ['height'] = text.size / 2 * (sum(scale) / len(scale))
-            ucs = UCS(origin=matrix.to_translation()).rotate(
-                (raa[1], raa[2], raa[3]), raa[0])
-            text_dxf = layout.add_text(text.body, dxfattribs)
-            text_dxf.set_pos((0, 0, 0), align=align_dxf)
-            text_dxf.transform(ucs.matrix)
-            return text_dxf
-
-        def entity_func_mtext():
+        
+        if self.exporter.settings.data_settings.texts_export == TextType.MTEXT.value:
             align_dxf = "MTEXT_"
             if align_y in ('TOP', 'BOTTOM'):
                 align_dxf += align_y + "_"
@@ -63,11 +45,22 @@ class TextManager(Manager):
             text_dxf.dxf.char_height = text.size / 2 * (sum(scale) / len(scale))
             text_dxf.set_location((0, 0, 0), attachment_point=align_dxf)
             text_dxf.transform(ucs.matrix)
-            return text_dxf
 
-        self.create_and_transform_entity(
-            entity_func_mtext if self.exporter.settings.data_settings.texts_export == TextType.MTEXT.value else entity_func_text,
-            True,
-            dxfattribs,
-            callback,
-            )
+        else:
+            align_dxf = ""
+            if align_y in ('TOP', 'BOTTOM'):
+                align_dxf = align_y + "_"
+            elif align_y == 'CENTER':
+                align_dxf = "MIDDLE_"
+            if align_x in ('LEFT', 'CENTER', 'RIGHT'):
+                align_dxf += align_x
+            else:
+                align_dxf += "CENTER"
+            dxfattribs ['height'] = text.size / 2 * (sum(scale) / len(scale))
+            ucs = UCS(origin=matrix.to_translation()).rotate(
+                (raa[1], raa[2], raa[3]), raa[0])
+            text_dxf = layout.add_text(text.body, dxfattribs)
+            text_dxf.set_pos((0, 0, 0), align=align_dxf)
+            text_dxf.transform(ucs.matrix)
+        
+        callback(text_dxf)
