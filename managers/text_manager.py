fd2030c7299bc07b3351844114cae2ecacf8b582
Gorgious56 <nathan.hild@gmail.com>
2021-05-28 14:11:41 +0200

    Text rotation now correct when child of object

--- managers/text_manager.py
+++ managers/text_manager.py
@@ -16,12 +16,15 @@ class TextManager(Manager):
         'MTEXT_BOTTOM_RIGHT' : 9,
     }
 
-    def write_text(self, layout, text_obj, matrix, raa, dxfattribs, callback):
+    def write_text(self, layout, text_obj, matrix, dxfattribs, callback):
         text = text_obj.data
         align_x, align_y = text.align_x, text.align_y
 
         scale = matrix.to_scale()
         # TODO : Use correct font : Need to know supported ones else file is corrupted
+        
+        # Is there no way to directly get an axis angle from matrix ??
+        raa = matrix.to_euler().to_quaternion().to_axis_angle()
         
         if self.exporter.settings.data_settings.texts_export == TextType.MTEXT.value:
             align_dxf = "MTEXT_"
@@ -40,8 +43,12 @@ class TextManager(Manager):
             align_dxf = self.MTEXT_ATTACHMENT_POINTS_DIC.get(align_dxf, 1)
 
             # Need to recreate a matrix for 3D location and rotation
+            
             ucs = UCS(origin=matrix.to_translation()).rotate(
-                (raa[1], raa[2], raa[3]), raa[0])
+                (raa[0][0], raa[0][1], raa[0][2]), raa[1])
+            # TODO : put this in math utils :
+            
+            
             text_dxf = layout.add_mtext(text.body, dxfattribs)
             text_dxf.dxf.char_height = text.size / 2 * (sum(scale) / len(scale))
             text_dxf.set_location((0, 0, 0), attachment_point=align_dxf)
@@ -61,7 +68,7 @@ class TextManager(Manager):
 
             # Need to recreate a matrix for 3D location and rotation
             ucs = UCS(origin=matrix.to_translation()).rotate(
-                (raa[1], raa[2], raa[3]), raa[0])
+                (raa[0][0], raa[0][1], raa[0][2]), raa[1])
             text_dxf = layout.add_text(text.body, dxfattribs)
             text_dxf.set_pos((0, 0, 0), align=align_dxf)
             text_dxf.transform(ucs.matrix)
