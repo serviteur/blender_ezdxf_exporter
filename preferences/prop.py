e302f9d502c98776c05577403a4b7b08b74ff992
Gorgious <nathan.hild@gmail.com>
2021-11-25 09:20:29 +0100

    Simplify aci palette code  #7

--- preferences/prop.py
+++ preferences/prop.py
@@ -1,17 +1,5 @@
 import bpy
 
 
-def set_prop(self, value):
-    if self.readonly:
-        return
-    self["value"] = value
-    self.readonly = True
-
-
-def get_prop(self):
-    return self["value"]
-
-
 class ColorPropertyGroup(bpy.types.PropertyGroup):
-    value: bpy.props.FloatVectorProperty(subtype="COLOR", set=set_prop, get=get_prop, min=0, max=1)
-    readonly: bpy.props.BoolProperty()
+    value: bpy.props.FloatVectorProperty(subtype="COLOR", set=lambda s, c: None, get=lambda s: s["value"], min=0, max=1)
