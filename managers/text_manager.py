778dcd5bc23225bf88b41e54361fa124671fa81f
Gorgious <nathan.hild@gmail.com>
2021-11-25 09:19:55 +0100

    Workaround for #9 : Allow text override only on windows If you want to use a custom font, you'll have to set it directly on the Text object's text propertes The default blender font will export as the Standard font in dxf

--- managers/text_manager.py
+++ managers/text_manager.py
@@ -1,21 +1,21 @@
 from ezdxf.math import UCS
 from ..settings.data_settings import TextType
-from ..settings.text_settings import FillColor
+from ..settings.text_settings import FillColor, FONTS_TTF
 from .manager import Manager
 import ntpath
 
 
 class TextManager(Manager):
     MTEXT_ATTACHMENT_POINTS_DIC = {
-        'MTEXT_TOP_LEFT': 1,
-        'MTEXT_TOP_CENTER': 2,
-        'MTEXT_TOP_RIGHT': 3,
-        'MTEXT_MIDDLE_LEFT': 4,
-        'MTEXT_MIDDLE_CENTER': 5,
-        'MTEXT_MIDDLE_RIGHT': 6,
-        'MTEXT_BOTTOM_LEFT': 7,
-        'MTEXT_BOTTOM_CENTER': 8,
-        'MTEXT_BOTTOM_RIGHT': 9,
+        "MTEXT_TOP_LEFT": 1,
+        "MTEXT_TOP_CENTER": 2,
+        "MTEXT_TOP_RIGHT": 3,
+        "MTEXT_MIDDLE_LEFT": 4,
+        "MTEXT_MIDDLE_CENTER": 5,
+        "MTEXT_MIDDLE_RIGHT": 6,
+        "MTEXT_BOTTOM_LEFT": 7,
+        "MTEXT_BOTTOM_CENTER": 8,
+        "MTEXT_BOTTOM_RIGHT": 9,
     }
 
     def write_text(self, layout, text_obj, matrix, dxfattribs, callback):
@@ -29,15 +29,15 @@ class TextManager(Manager):
 
         if self.exporter.settings.data_settings.texts_export == TextType.MTEXT.value:
             align_dxf = "MTEXT_"
-            if align_y in ('TOP', 'BOTTOM'):
+            if align_y in ("TOP", "BOTTOM"):
                 align_dxf += align_y + "_"
-            elif align_y == 'CENTER':
+            elif align_y == "CENTER":
                 align_dxf += "MIDDLE_"
             elif align_y.startswith("TOP"):
                 align_dxf += "TOP_"
             elif align_y.startswith("BOTTOM"):
                 align_dxf += "BOTTOM_"
-            if align_x in ('LEFT', 'CENTER', 'RIGHT'):
+            if align_x in ("LEFT", "CENTER", "RIGHT"):
                 align_dxf += align_x
             else:
                 align_dxf += "CENTER"
@@ -45,49 +45,47 @@ class TextManager(Manager):
 
             # Need to recreate a matrix for 3D location and rotation
             # TODO : put this in math utils :
-            ucs = UCS(origin=matrix.to_translation()).rotate(
-                (raa[0][0], raa[0][1], raa[0][2]), raa[1])
+            ucs = UCS(origin=matrix.to_translation()).rotate((raa[0][0], raa[0][1], raa[0][2]), raa[1])
 
             text_settings = self.exporter.settings.text_settings
             text_dxf = layout.add_mtext(text.body, dxfattribs)
-            text_dxf.dxf.char_height = text.size / \
-                2 * (sum(scale) / len(scale))
+            text_dxf.dxf.char_height = text.size / 2 * (sum(scale) / len(scale))
 
             # Set Font. If necessary, create a new style per font
             text_font_path = text.font.filepath
-            if text_settings.font_override or text_font_path == "<builtin>":  # Override or default blender font
+            if FONTS_TTF and (
+                text_settings.font_override or text_font_path == "<builtin>"
+            ):  # Override or default blender font
                 dxf_font_name = text_settings.font_name
             else:
                 dxf_font_name = ntpath.basename(text_font_path)
 
-            if dxf_font_name not in self.exporter.doc.styles:
-                self.exporter.doc.styles.new(dxf_font_name, dxfattribs={
-                                             "font": dxf_font_name})
+            if dxf_font_name != "<builtin>":
+                if dxf_font_name not in self.exporter.doc.styles:
+                    self.exporter.doc.styles.new(dxf_font_name, dxfattribs={"font": dxf_font_name})
 
-            text_dxf.dxf.style = dxf_font_name
+                text_dxf.dxf.style = dxf_font_name
             text_dxf.set_location((0, 0, 0), attachment_point=align_dxf)
 
             # Set Fill
             if text_settings.fill_type != FillColor.NONE.value:
-                text_dxf.set_bg_color(
-                    text_settings.get_color_value(), scale=text_settings.fill_scale)
+                text_dxf.set_bg_color(text_settings.get_color_value(), scale=text_settings.fill_scale)
 
         else:
             # Couldn't make custom fonts work with Text object. Use Mtext instead.
             align_dxf = ""
-            if align_y in ('TOP', 'BOTTOM'):
+            if align_y in ("TOP", "BOTTOM"):
                 align_dxf = align_y + "_"
-            elif align_y == 'CENTER':
+            elif align_y == "CENTER":
                 align_dxf = "MIDDLE_"
-            if align_x in ('LEFT', 'CENTER', 'RIGHT'):
+            if align_x in ("LEFT", "CENTER", "RIGHT"):
                 align_dxf += align_x
             else:
                 align_dxf += "CENTER"
-            dxfattribs['height'] = text.size / 2 * (sum(scale) / len(scale))
+            dxfattribs["height"] = text.size / 2 * (sum(scale) / len(scale))
 
             # Need to recreate a matrix for 3D location and rotation
-            ucs = UCS(origin=matrix.to_translation()).rotate(
-                (raa[0][0], raa[0][1], raa[0][2]), raa[1])
+            ucs = UCS(origin=matrix.to_translation()).rotate((raa[0][0], raa[0][1], raa[0][2]), raa[1])
             text_dxf = layout.add_text(text.body, dxfattribs)
             text_dxf.set_pos((0, 0, 0), align=align_dxf)
         text_dxf.transform(ucs.matrix)
--- settings/text_settings.py
+++ settings/text_settings.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import os
 from os import listdir
 from os.path import isfile, join, splitext
 
@@ -19,9 +20,9 @@ from ..shared_maths import get_256_rgb_a
 
 class TextType(Enum):
     NONE = NO_EXPORT
-    MTEXT = 'MText'
-    TEXT = 'Text'
-    MESH = 'Mesh'
+    MTEXT = "MText"
+    TEXT = "Text"
+    MESH = "Mesh"
 
 
 class FillColor(Enum):
@@ -31,16 +32,21 @@ class FillColor(Enum):
     BACKGROUND = "Background Color"
 
 
-FONTS_TTF = [(f, splitext(f)[0].capitalize(), "") for f in listdir("C:/Windows/Fonts") if isfile(join("C:/Windows/Fonts", f)) if splitext(f)[1] == '.ttf']
+FONTS_TTF = (
+    [
+        (f, splitext(f)[0].capitalize(), "")
+        for f in listdir("C:/Windows/Fonts")
+        if isfile(join("C:/Windows/Fonts", f))
+        if splitext(f)[1] == ".ttf"
+    ]
+    if os.name == "nt"
+    else []
+)  # TODO : support override fonts for Linux and OSx
 
 
 class TextSettings(PropertyGroup):
-    fill_type: EnumProperty(
-        name="Fill",
-        default=FillColor.NONE.value,
-        items=[(f_c.value,)*3 for f_c in FillColor])
-    fill_color_rgb: FloatVectorProperty(
-        name="Fill Color", default=(1, 1, 1), min=0, max=1, subtype='COLOR')
+    fill_type: EnumProperty(name="Fill", default=FillColor.NONE.value, items=[(f_c.value,) * 3 for f_c in FillColor])
+    fill_color_rgb: FloatVectorProperty(name="Fill Color", default=(1, 1, 1), min=0, max=1, subtype="COLOR")
     fill_color_aci: EnumProperty(
         name="Fill Color",
         description="Autocad Color Index - Color as an integer [0:255]",
@@ -48,11 +54,8 @@ class TextSettings(PropertyGroup):
         items=[aci.value for aci in ACIColor],
     )
     fill_scale: FloatProperty(
-        name="Fill Scale",
-        description="1=Exact Fit, 1.5 = default, 5=Max",
-        default=1.5,
-        min=1,
-        max=5)
+        name="Fill Scale", description="1=Exact Fit, 1.5 = default, 5=Max", default=1.5, min=1, max=5
+    )
     font_name: EnumProperty(
         name="Font",
         description="Default font to use (Bfont is automatically set to this)",
@@ -70,12 +73,13 @@ class TextSettings(PropertyGroup):
         elif self.fill_type == FillColor.ACI.value:
             return int(self.fill_color_aci)
         elif self.fill_type == FillColor.BACKGROUND.value:
-            return 'canvas'
+            return "canvas"
         return None
 
     def draw(self, layout):
         box = layout.box()
-        box.prop(self, "fill_type")
+        box.label(text="Text Options")
+        box.prop(self, "fill_type", text="Fill")
 
         if self.fill_type != FillColor.NONE.value:
             fill_row = box.row()
@@ -86,6 +90,9 @@ class TextSettings(PropertyGroup):
             if self.fill_type != FillColor.NONE.value:
                 fill_row.prop(self, "fill_scale", text="Scale")
 
-        font_row = box.split(factor=0.65, align=True)
-        font_row.prop(self, "font_name")
-        font_row.prop(self, "font_override", text="Override", toggle=True)
+        if os.name == "nt":  # TODO : support override fonts for Linux and OSx
+            font_row = box.split(factor=0.65, align=True)
+            font_row.prop(self, "font_name")
+            font_row.prop(self, "font_override", text="Override", toggle=True)
+        else:
+            self.font_override = False
