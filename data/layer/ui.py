4a60436905771e60eac8918cd4117fe45ba8855e
Gorgious <nathan.hild@gmail.com>
2021-11-30 11:13:50 +0100

    Rename layer settings parameter

--- data/layer/ui.py
+++ data/layer/ui.py
@@ -16,38 +16,44 @@ def draw_global(self, layout):
     mat_layer_link.active = self.material_layer_export
 
 
+def draw_local(layer_settings, layout, context, obj_name=None):
     prefs = get_preferences(context).layer_preferences
     if obj_name is None:
         obj_name = "Default Object"
     layout.label(text=obj_name + " Layer")
     layer_box = layout.box()
-    layer_box.prop(self, "entity_layer_to", text="")
+    layer_box.prop(layer_settings, "entity_layer_to", text="")
     row = layer_box.row()
-    use_prefs_prefix_suffix = self.entity_layer_preferences_prefix_suffix
+    use_prefs_prefix_suffix = layer_settings.entity_layer_preferences_prefix_suffix
     if use_prefs_prefix_suffix:
         sub_row = row.row()
         sub_row.prop(prefs, "layer_prefix", text="Prefix")
         sub_row.prop(prefs, "layer_suffix", text="Suffix")
-        sub_row.enabled=False
+        sub_row.enabled = False
     else:
-        row.prop(self, "entity_layer_prefix", text="Prefix")
-        row.prop(self, "entity_layer_suffix", text="Suffix")
-    row.prop(self, "entity_layer_preferences_prefix_suffix", icon="LINKED" if use_prefs_prefix_suffix else "UNLINKED", text="",)
+        row.prop(layer_settings, "entity_layer_prefix", text="Prefix")
+        row.prop(layer_settings, "entity_layer_suffix", text="Suffix")
+    row.prop(
+        layer_settings,
+        "entity_layer_preferences_prefix_suffix",
+        icon="LINKED" if use_prefs_prefix_suffix else "UNLINKED",
+        text="",
+    )
     layer_color_split = layer_box.split(factor=0.5)
-    layer_color_split.prop(self, "entity_layer_color")
+    layer_color_split.prop(layer_settings, "entity_layer_color")
     layer_setting = layer_color_split.row()
-    layer_setting.prop(self, "entity_layer_transparency")
+    layer_setting.prop(layer_settings, "entity_layer_transparency")
     layer_setting.active = (
-        self.entity_layer_color != "2"
-        and self.entity_layer_to
+        layer_settings.entity_layer_color != "2"
+        and layer_settings.entity_layer_to
         in (
             EntityLayer.OBJECT_NAME.value,
             EntityLayer.MATERIAL.value,
         )
-    ) or self.entity_layer_color == "1"
-    if self.entity_layer_color == "1":
-        layer_box.prop(self, "entity_layer_color_custom_prop_name")
-    layer_box.prop(self, "entity_layer_separate")
+    ) or layer_settings.entity_layer_color == "1"
+    if layer_settings.entity_layer_color == "1":
+        layer_box.prop(layer_settings, "entity_layer_color_custom_prop_name")
+    layer_box.prop(layer_settings, "entity_layer_separate")
 
     return layer_box
 
