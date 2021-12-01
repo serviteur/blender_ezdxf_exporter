e59a6b2b27973f0a941df05640fd262052e130f4
Gorgious <nathan.hild@gmail.com>
2021-12-01 11:15:23 +0100

    rename settings parameter

--- data/text/ui.py
+++ data/text/ui.py
@@ -2,23 +2,23 @@ import os
 from .constants import FillColor
 
 
-def draw(self, layout):
+def draw(text_data, layout):
     box = layout.box()
     box.label(text="Text Options")
-    box.prop(self, "fill_type", text="Fill")
+    box.prop(text_data, "fill_type", text="Fill")
 
-    if self.fill_type != FillColor.NONE.value:
+    if text_data.fill_type != FillColor.NONE.value:
         fill_row = box.row()
-        if self.fill_type == FillColor.CUSTOM.value:
-            fill_row.prop(self, "fill_color_rgb", text="")
-        if self.fill_type == FillColor.ACI.value:
-            fill_row.prop(self, "fill_color_aci", text="")
-        if self.fill_type != FillColor.NONE.value:
-            fill_row.prop(self, "fill_scale", text="Scale")
+        if text_data.fill_type == FillColor.CUSTOM.value:
+            fill_row.prop(text_data, "fill_color_rgb", text="")
+        if text_data.fill_type == FillColor.ACI.value:
+            fill_row.prop(text_data, "fill_color_aci", text="")
+        if text_data.fill_type != FillColor.NONE.value:
+            fill_row.prop(text_data, "fill_scale", text="Scale")
 
     if os.name == "nt":  # TODO : support override fonts for Linux and OSx
         font_row = box.split(factor=0.65, align=True)
-        font_row.prop(self, "font_name")
-        font_row.prop(self, "font_override", text="Override", toggle=True)
+        font_row.prop(text_data, "font_name")
+        font_row.prop(text_data, "font_override", text="Override", toggle=True)
     else:
         self.font_override = False
